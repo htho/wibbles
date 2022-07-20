0d3c2cd2e531833ba319bc95b6f9fc0a5e631176
Hauke Thorenz <htho@thorenz.net>
2022-07-20 20:33:19 +0200

    setup eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+    root: true,
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+      tsconfigRootDir: __dirname,
+      project: ['./tsconfig.json', './tsconfig.eslint.json'],
+    },
+    plugins: ['@typescript-eslint'],
+    extends: [
+      'eslint:recommended',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    ],
+  };
