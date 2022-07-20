314e9faf450f434c0b8833a72a36331850a278ef
Hauke Thorenz <htho@thorenz.net>
2022-07-20 20:39:45 +0200

    add eslint-plugin-jest

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,21 @@
+// eslint-disable-next-line no-undef
 module.exports = {
     root: true,
     parser: '@typescript-eslint/parser',
     parserOptions: {
+      // eslint-disable-next-line no-undef
       tsconfigRootDir: __dirname,
       project: ['./tsconfig.json', './tsconfig.eslint.json'],
     },
-    plugins: ['@typescript-eslint'],
+    plugins: [
+      '@typescript-eslint',
+      'jest',
+    ],
     extends: [
       'eslint:recommended',
       'plugin:@typescript-eslint/recommended',
       'plugin:@typescript-eslint/recommended-requiring-type-checking',
+      'plugin:jest/recommended',
     ],
+
   };
